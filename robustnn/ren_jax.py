d23a2cf89e53176740f8fea42810b1e1a5442fd7
nic-barbara <nbar5346@uni.sydney.edu.au>
2024-06-26 18:28:01 +1000

    Switched activation func to an input, completed custom VJP for REN

--- robustnn/ren_jax.py
+++ robustnn/ren_jax.py
@@ -1,15 +1,15 @@
 import jax
 import jax.numpy as jnp
 
-def activation(x):
-    return jnp.tanh(x)
+from functools import partial
 
-@jax.jit
-def solve_tril_layer(D11, b):
+
+@partial(jax.jit, static_argnums=(0,))
+def solve_tril_layer(activation, D11, b):
     """
     Solve `w = activation(D11 @ w + b)` for lower-triangular D11.
     
-    Activation must be monotone with slope restricted to `[0,1]`.
+    Only valid for the forward pass (not backprop with auto-diff).
     """
     w_eq = jnp.zeros_like(b)
     D11_T = D11.T
@@ -20,3 +20,62 @@ def solve_tril_layer(D11, b):
         Di_wi = wi @ Di_T
         w_eq = w_eq.at[..., i].set(activation(Di_wi + bi))
     return w_eq
+
+
+def tril_equlibrium_layer(activation, D11, b):
+    """
+    Solve `w = activation(D11 @ w + b)` for lower-triangular D11.
+    
+    Activation must be monotone with slope restricted to `[0,1]`.
+    """
+    # Forward pass
+    w_eq = jax.lax.stop_gradient(solve_tril_layer(activation, D11, b))
+    
+    # Re-evaluate the equilibrium layer so autodiff can track grads
+    # through these two operations, then customise for grad of w_eq
+    v = w_eq @ D11.T + b
+    w_eq = activation(v)
+    return tril_layer_do_grad(activation, D11, v, w_eq)
+
+
+@partial(jax.custom_vjp, nondiff_argnums=(0,))
+def tril_layer_do_grad(activation, D11, v, w_eq):
+    return w_eq
+
+
+def tril_layer_do_grad_fwd(activation, D11, v, w_eq):
+    I = jnp.identity(v.shape[-1])
+    return w_eq, (D11, v, I)
+
+
+def tril_layer_do_grad_bwd(activation, res, y_bar):
+    """
+    Compute backwards pass with implicit function theorem.
+    
+    See Equation 13 of Revay et al. (2023).
+    """
+    D11, v, I = res
+    
+    # Ignore grads for D11, v
+    D11_bar = jnp.zeros_like(D11)
+    v_bar = jnp.zeros_like(v)
+    
+    # Get Jacobian of activation(v) evaluated at v
+    # Scalar activation ==> diagonal Jacobian, so get
+    # diagonal elements for each batch. j_diag has
+    # dimensions (batches, nv)
+    _, vjp_act_v = jax.vjp(activation, v)
+    j_diag, = vjp_act_v(jnp.ones_like(v))
+    
+    # Compute gradient with implicit function theorem (per batch)
+    w_eq_bar = jnp.zeros_like(v)
+    for i in range(w_eq_bar.shape[0]):
+        ji = j_diag[i, ...]
+        y_bar_i = y_bar[i, ...]
+        w_grad = jnp.linalg.solve(I - (ji * D11.T), y_bar_i.T).T
+        w_eq_bar = w_eq_bar.at[i, ...].set(w_grad)
+    
+    return (D11_bar, v_bar, w_eq_bar)
+
+
+tril_layer_do_grad.defvjp(tril_layer_do_grad_fwd, tril_layer_do_grad_bwd)
--- scripts/test.py
+++ scripts/test.py
@@ -1,128 +1,10 @@
 import jax
 import jax.numpy as jnp
 
-from functools import partial
-from jax import custom_vjp
-from jax import grad, vjp
-
-import flax.linen as nn
-
-from time import time, time_ns
-
-# from robustnn.ren_jax import solve_tril_layer
-
-
-### Vector-Jacobian product (reverse mode)
-
-# f :: a -> b
-@custom_vjp
-def f(x):
-  return jnp.sin(x)
-
-# f_fwd :: a -> (b, c)
-def f_fwd(x):
-  return f(x), (jnp.cos(x),)
-
-# f_bwd :: (c, CT b) -> CT a
-def f_bwd(res, y_bar):
-  cos_x, = res
-  return (cos_x * y_bar,)
-
-f.defvjp(f_fwd, f_bwd)
-
-
-##################### Custom for REN #####################
-
-def activation(x):
-    return jnp.tanh(x)
-  
-# TODO: Add activation function as an input to all of this!
-#       I think sticking with functional programming here
-#       actually is fine.
-
-# @jax.jit
-def solve_tril_layer(D11, b):
-    """
-    Solve `w = activation(D11 @ w + b)` for lower-triangular D11.
-    
-    Only valid for the forward pass (not backprop with auto-diff).
-    """
-    w_eq = jnp.zeros_like(b)
-    D11_T = D11.T
-    for i in range(D11.shape[0]):
-        Di_T = D11_T[:i, i]
-        wi = w_eq[..., :i]
-        bi = b[..., i]
-        Di_wi = wi @ Di_T
-        w_eq = w_eq.at[..., i].set(activation(Di_wi + bi))
-    return w_eq
-
-def tril_equlibrium_layer(D11, b):
-    """
-    Solve `w = activation(D11 @ w + b)` for lower-triangular D11.
-    
-    Activation must be monotone with slope restricted to `[0,1]`.
-    """
-    
-    # Solve the equilibrium layer, forward pass only
-    w_eq = jax.lax.stop_gradient(
-        solve_tril_layer(D11, b)
-    )
-    
-    # Re-evaluate the equilibrium layer so autodiff can track grads
-    # through these two operations, then customise for grad of w_eq
-    v = w_eq @ D11.T + b
-    w_eq = activation(v)
-    # return tril_layer_do_grad(D11, v, w_eq)
-    return tril_layer_do_grad_fwd(D11, v, w_eq)
-
-def tril_layer_do_grad(D11, v, w_eq):
-    """Dummy forward pass for equilibrium layers."""
-    return w_eq
-  
-def tril_layer_do_grad_fwd(D11, v, w_eq):
-    I = jnp.identity(v.shape[-1])
-    return w_eq, (D11, v, I)
-
-def tril_layer_do_grad_bwd(res, y_bar):
-    """
-    Compute backwards pass with implicit function theorem.
-    
-    See Equation 13 of Revay et al. (2023).
-    """
-    D11, v, I = res
-    
-    # Ignore grads for D11, v
-    D11_bar = jnp.zeros_like(D11)
-    v_bar = jnp.zeros_like(v)
-    
-    # Get Jacobian of activation(v) evaluated at v
-    # Scalar activation ==> diagonal Jacobian, so get
-    # diagonal elements for each batch. j_diag has
-    # dimensions (batches, nv)
-    _, vjp_act_v = vjp(activation, v)
-    j_diag, = vjp_act_v(jnp.ones_like(v))
-    
-    # Compute gradient with implicit function theorem (per batch)
-    w_eq_bar = jnp.zeros_like(v)
-    for i in range(w_eq_bar.shape[0]):
-        ji = j_diag[i, ...]
-        y_bar_i = y_bar[i, ...]
-        temp = jnp.linalg.solve(I - (ji * D11.T), y_bar_i.T)
-        w_eq_bar = w_eq_bar.at[i, ...].set(temp.T)
-    
-    return (D11_bar, v_bar, w_eq_bar)
-    
+from robustnn.ren_jax import tril_equlibrium_layer
 
 
 ##################### Test it all out #####################
-
-nv, batches = 16, 128
-rng = jax.random.PRNGKey(0)
-rng1, rng2 = jax.random.split(rng)
-# b = jax.random.normal(rng2, (batches, nv))
-# D = jax.random.normal(rng1, (nv, nv))
-# D = jnp.tril(D, k=-1)
 
 D = jnp.array(
     [[0.0 , 0.        , 0.        , 0.        , 0.        ],
@@ -136,32 +18,20 @@ b = jnp.array(
     [0.20438278, 0.7415488 , 0.15026295, 0.21696508, 0.32493377],
     [0.7355863 , 0.79253435, 0.3715024 , 0.1306243 , 0.04838264]]
 )
-# b = jnp.array([[0.5338    , -0.9719182 , 0.61623883, -0.868845  , 0.6309322 ]])
 
 # Run it once
-w_eq = solve_tril_layer(D, b)
-# print(w_eq)
-
-# # Time and test
-# # Need to jit the bloody test function so everything stays on the GPU...
-# @jax.jit
-# def test_func():
-#     old = solve_tril_layer(D, b)
-#     for _ in range(N):
-#         out = solve_tril_layer(D, b)
-#         results = out - old
-#         old = out
-#     return results
-
-# N=10000
-# test_func()
-# start = time_ns()
-# out = test_func()
-# dt = ((time_ns() - start) / N)
-# print("Time taken (nanosec): ", dt)
-
+act = jnp.tanh
+w_eq = tril_equlibrium_layer(act, D, b)
+print(w_eq)
 
 # Test gradients
-out, res = tril_equlibrium_layer(D, b)
-out = tril_layer_do_grad_bwd(res, jnp.ones_like(w_eq))
-print(out[2])
+def loss(D, b):
+    w_eq = tril_equlibrium_layer(act, D, b)
+    return jnp.sum(w_eq**2)
+
+grad_func = jax.jit(jax.grad(loss, argnums=(0,1)))
+gs = grad_func(D,b)
+
+print(loss(D,b))
+print("Gradient for D: ", gs[0])
+print("Gradient for b: ", gs[1])
