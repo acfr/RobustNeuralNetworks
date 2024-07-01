e02b55e2502d6299b1067d46ebb4fdb2c4cc9c1a
nic-barbara <nbar5346@uni.sydney.edu.au>
2024-07-01 15:41:11 +1000

    Removed custom backwards pass

--- robustnn/ren_jax/ren_base.py
+++ robustnn/ren_jax/ren_base.py
@@ -153,22 +153,16 @@ class RENBase(nn.Module):
             _rng = jax.random.PRNGKey(0)
             D22 = init.zeros(_rng, (ny, nu), self.param_dtype)
         
-        if not self.d22_free and not self.d22_zero:
-            d = min(nu, ny)
-            X3 = self.param("X3", identity_init(), (d, d), self.param_dtype)
-            Y3 = self.param("Y3", init.zeros_init(), (d, d), self.param_dtype)
-            Z3 = self.param("Z3", init.zeros_init(), (abs(ny - nu), d), 
-                            self.param_dtype)
-        else:
-            X3 = None
-            Y3 = None
-            Z3 = None
+        d = min(nu, ny)
+        X3 = self.param("X3", identity_init(), (d, d), self.param_dtype)
+        Y3 = self.param("Y3", init.zeros_init(), (d, d), self.param_dtype)
+        Z3 = self.param("Z3", init.zeros_init(), (abs(ny - nu), d), 
+                        self.param_dtype)
             
         # Set up the direct parameter struct
         self.direct = DirectRENParams(p, X, B2, D12, Y1, C2, D21, 
                                       D22, X3, Y3, Z3, bx, bv, by)
-        
-    @nn.compact
+
     def __call__(self, state: Array, inputs: Array) -> Tuple[Array, Array]:
         """
         Call an REN.
@@ -177,7 +171,7 @@ class RENBase(nn.Module):
         at a single time. The syntax is `state, out = ren(state, in)` where `state` 
         is an array.
         """
-
+        
         # Direct parameterisation mapping
         explicit = self.direct_to_explicit(self.direct)
         
@@ -272,29 +266,14 @@ class RENBase(nn.Module):
         rng, _ = jax.random.split(rng)
         mem_shape = batch_dims + (self.state_size,)
         return self.carry_init(rng, mem_shape, self.param_dtype)
+    
 
-
+@partial(jax.jit, static_argnums=(0,))
 def tril_equlibrium_layer(activation, D11, b):
     """
     Solve `w = activation(D11 @ w + b)` for lower-triangular D11.
     
     Activation must be monotone with slope restricted to `[0,1]`.
-    """
-    # Forward pass (compute custom grads below)
-    w_eq = jax.lax.stop_gradient(solve_tril_layer(activation, D11, b))
-    
-    # Re-evaluate the equilibrium layer so autodiff can track grads
-    # through these two operations, then customise for grad of w_eq
-    v = w_eq @ D11.T + b
-    w_eq = activation(v)
-    return tril_layer_do_grad(activation, D11, v, w_eq)
-
-@partial(jax.jit, static_argnums=(0,))
-def solve_tril_layer(activation, D11, b):
-    """
-    Solve `w = activation(D11 @ w + b)` for lower-triangular D11.
-    
-    Only valid for the forward pass (not backprop with auto-diff).
     """
     w_eq = jnp.zeros_like(b)
     D11_T = D11.T
@@ -305,42 +284,3 @@ def solve_tril_layer(activation, D11, b):
         Di_wi = wi @ Di_T
         w_eq = w_eq.at[..., i].set(activation(Di_wi + bi))
     return w_eq
-
-@partial(jax.custom_vjp, nondiff_argnums=(0,))
-def tril_layer_do_grad(activation, D11, v, w_eq):
-    return w_eq
-
-def tril_layer_do_grad_fwd(activation, D11, v, w_eq):
-    I = jnp.identity(v.shape[-1])
-    return w_eq, (D11, v, I)
-
-def tril_layer_do_grad_bwd(activation, res, y_bar):
-    """
-    Compute backwards pass with implicit function theorem.
-    
-    See Equation 13 of Revay et al. (2023).
-    """
-    D11, v, I = res
-    
-    # Ignore grads for D11, v
-    D11_bar = jnp.zeros_like(D11)
-    v_bar = jnp.zeros_like(v)
-    
-    # Get Jacobian of activation(v) evaluated at v
-    # Scalar activation ==> diagonal Jacobian, so get
-    # diagonal elements for each batch. j_diag has
-    # dimensions (batches, nv)
-    _, vjp_act_v = jax.vjp(activation, v)
-    j_diag, = vjp_act_v(jnp.ones_like(v))
-    
-    # Compute gradient with implicit function theorem (per batch)
-    w_eq_bar = jnp.zeros_like(v)
-    for i in range(w_eq_bar.shape[0]):
-        ji = j_diag[i, ...]
-        y_bar_i = y_bar[i, ...]
-        w_grad = jnp.linalg.solve(I - (ji * D11.T), y_bar_i.T).T
-        w_eq_bar = w_eq_bar.at[i, ...].set(w_grad)
-    
-    return (D11_bar, v_bar, w_eq_bar)
-
-tril_layer_do_grad.defvjp(tril_layer_do_grad_fwd, tril_layer_do_grad_bwd)
